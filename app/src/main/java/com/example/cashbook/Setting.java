f1e32665a6c1b6bd2ebf1b89d63429c4350ade65
Hritvik Gupta <hritvik7654@gmail.com>
2022-05-28 06:51:40 +0530

    commit

--- app/src/main/java/com/example/cashbook/Setting.java
+++ app/src/main/java/com/example/cashbook/Setting.java
@@ -6,14 +6,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.time.temporal.Temporal;
 
 public class Setting extends AppCompatActivity implements BottomFragment.options {
+
+    ImageView personImage, editUser, night;
+    TextView nameId, numId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
         setTitle("Settings");
+
+        personImage = findViewById(R.id.personImage);
+        nameId = findViewById(R.id.nameId);
+        numId = findViewById(R.id.numId);
+        editUser = findViewById(R.id.editUser);
+        night = findViewById(R.id.night);
+        editUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nameId.setText("Hritvik Gupta");
+                numId.setText("954534534");
+            }
+        });
+        night.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setTheme(R.style.dartTheme);
+            }
+        });
+
+
+
     }
 
     @Override
